32778f3b220df19e7b153938f3bca11580c71eaa
Henrique Lopes <henriquelopes28@outlook.com>
2024-05-27 19:14:39 -0300

    mudancas nos comentarios exponenciacao_03.asm

--- Exponenciacao/exponenciacao_03.asm
+++ Exponenciacao/exponenciacao_03.asm
@@ -5,31 +5,31 @@
 .text
 .globl main
 
-main:
+main:	
 	li $v0, 4
-	la $a0, entrada_x
+	la $a0, entrada_x # printa a label entrada_x
 	syscall
 	
-	li $v0, 5
+	li $v0, 5 # scanf entrada x
 	syscall
 	
-	la $a2, ($v0)
+	la $a2, ($v0) 
 	
 	li $v0, 4
-	la $a0, entrada_exp
+	la $a0, entrada_exp # printa a label entrada_exp
 	syscall
 	
-	li $v0, 5
+	li $v0, 5 # scanf entrada exp
 	syscall
 	
 	la $a1, ($v0)
 	
-	jal power_base2
+	jal power_base2 # chama a funcao
 	
 	move $t1, $v0 # t1 recebe o returno da funcao power
 	
 	li $v0, 4
-	la $a0, resultado # printa o resultado
+	la $a0, resultado # printa a label resultado
 	syscall
 	
 	li $v0, 1
@@ -43,21 +43,21 @@ main:
 
 power_base2:
 	
-	beq $a1, 0, exp_0
-	beq $a1, 1, exp_1
+	beq $a1, 0, exp_0 # if exp == 0 j exp_0
+	beq $a1, 1, exp_1 # if exp == 1 j exp_1
 	
-	li $v0, 2
+	li $v0, 2 
 	 
-	subi $a1, $a1, 1
-	sllv $v0, $v0, $a1
-	mul $v0, $v0, $a2
+	subi $a1, $a1, 1   # exp -= 1
+	sllv $v0, $v0, $a1 # v0 = 2 << exp
+	mul $v0, $v0, $a2  # v0 *= x
 	jr $ra
 	
 	exp_0:
-	move $v0, $a2
+	move $v0, $a2 # return x
 	jr $ra
 	
 	exp_1:
-	mul $v0, $a2, 2
+	mul $v0, $a2, 2 # return x * 2
 	jr $ra
 	
